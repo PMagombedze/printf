6f8c7a3dd847c9f530c7bfecb5baeac41a3d9d39
PMagombedze <percymagoras@outlook.com>
2023-04-25 17:35:03 +0200

    _printf_

--- _printf.c
+++ _printf.c
@@ -11,9 +11,9 @@
 int _printf(const char *format, ...)
 {
 va_list arg;
-int done;
+int i;
 va_start(arg, format);
-done = vfprintf(stdout, format, arg);
+i = vfprintf(stdout, format, arg);
 va_end(arg);
-return (done);
+return (i);
 }
